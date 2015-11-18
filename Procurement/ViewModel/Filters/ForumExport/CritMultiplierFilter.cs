feeb21c9e45dad07990e8e908c36672e2c01c1de
animalnots <animalnots@live.com>
2015-11-18 08:49:01 +0000

    - Добавлена поддержка новых лиг
    En:
    - Added support for new leagues

---
+++ Procurement/ViewModel/Filters/ForumExport/CritMultiplierFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class CritMultiplierFilter : ExplicitModBase
+    {
+        public CritMultiplierFilter()
+            : base(Lang.increasedCriticalMultiplier)
+        { }
+
+        public override bool CanFormCategory
+        {
+            get { return false; }
+        }
+
+        public override string Keyword
+        {
+            get { return Lang.increasedCriticalMultiplier; }
+        }
+
+        public override string Help
+        {
+            get { return Lang.increasedCriticalMultiplier; }
+        }
+
+        public override FilterGroup Group
+        {
+            get { return FilterGroup.Crit; }
+        }
+    }
+}
