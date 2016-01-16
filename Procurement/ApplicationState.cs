d2bb0d39f7e3d2ef7ede6036bbc89079ae023957
animalnots <animalnots@live.com>
2016-01-16 17:50:38 +0000

    Добавлены предметы из Патча 2.1.1с

--- Procurement/ApplicationState.cs
+++ Procurement/ApplicationState.cs
@@ -10,7 +10,7 @@ namespace Procurement
 {
     public static class ApplicationState
     {
-        public static string Version = "Прокьюремент 2.1.0";
+        public static string Version = "Прокьюремент 2.1.1";
         public static POEModel Model = new POEModel();
         public static Dictionary<string, Stash> Stash = new Dictionary<string, Stash>();
         public static Dictionary<string, Item> Inventory = new Dictionary<string, Item>();
